86a4fbfee98eba3895f8dccd11aa26aade20f91f
Matteo Garibaldi <matteo.garibaldi@gmail.com>
2023-04-16 21:57:54 +0200

    first commit

---
+++ src/app/asset.ts
@@ -0,0 +1,16 @@
+export class Asset {
+    name: string | undefined;
+    currentPrice: number;
+    number: number;
+    totalPrice: number;
+
+    constructor() {
+        this.name = "";
+        this.currentPrice = 0;
+        this.number = 1;
+        this.totalPrice = 0;
+    }
+
+
+
+}
---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { ETF } from './ETF';
+import { Asset } from '../asset';
+
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css']
+})
+export class PortfolioComponent implements OnInit {
+
+  numberAssets = 0;
+  totalValue = 0;
+
+  options = ETF
+
+  assetList: Asset[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.assetList[0] = new Asset();
+
+  }
+
+  public onInput(e: any, pos: number): void {
+    let find = this.options.find(x => x?.Name === e.target.value);
+    console.log(find?.Ticker);
+    this.assetList[pos].name = find?.Name;
+    this.assetList[pos].currentPrice = pos + 1 * 100;
+    this.updateTotal(pos);
+    this.calculateTotalQuantity();
+  }
+
+  public updateTotal(pos: number) {
+    this.assetList[pos].totalPrice = this.assetList[pos].number * this.assetList[pos].currentPrice;
+  }
+
+  calculateTotalQuantity() {
+    this.totalValue = 0;
+    this.assetList.forEach((lot: any) => {
+      this.totalValue += lot.totalPrice;
+    });
+  }
+
+  row = [
+    {
+      id: '',
+      name: '',
+      email: ''
+    }
+  ];
+
+  addTable() {
+    const obj = {
+      id: '',
+      name: '',
+      email: ''
+    }
+    this.row.push(obj)
+    this.numberAssets += 1;
+    this.assetList[this.numberAssets] = new Asset();
+  }
+
+  deleteRow(x: number) {
+    var delBtn = confirm(" Do you want to remove the asset ?");
+    if (delBtn == true) {
+      this.row.splice(x, 1);
+      this.assetList.slice(x, 1);
+      this.numberAssets -=  1;
+      this.calculateTotalQuantity();
+    }
+
+  }
+
+}
